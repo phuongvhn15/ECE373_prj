1d39540d8bf474d645d75f143d62eddcb1b587e6
minhminhminh1509 <76503692+minhminhminh1509@users.noreply.github.com>
2022-01-07 21:25:04 +0700

    Update MCP2515_linux_module.c

--- Team_1/Final_project/MCP2515_linux_module.c
+++ Team_1/Final_project/MCP2515_linux_module.c
@@ -93,19 +93,19 @@ static int __init ModuleInit(void) {
 	// gpio_set_value(24,1);
 	// mdelay(2000);
 
-	gpio_set_value(24,0);
+	//gpio_set_value(24,0);
 	u8 tx_val1[] = {0x2, 0x0F, 0x31};
 	u8 rx_val;
 	printk("a %d", rx_val);
 	
 	spi_write(mcp2515_dev, tx_val1, 3);
-	gpio_set_value(24,1);
+	//gpio_set_value(24,1);
 
-	gpio_set_value(24,0);
+	//gpio_set_value(24,0);
 	u8 tx_val2[] = {0x03, 0x0F};
-	//spi_write_then_read(mcp2515_dev, tx_val2, 2, &rx_val, 1);
+	spi_write_then_read(mcp2515_dev, tx_val2, 2, &rx_val, 1);
 	
-	gpio_set_value(24,1);
+	//gpio_set_value(24,1);
 	printk("b %d", rx_val);
 	// mcp251x_write_reg(mcp2515_dev, CANSTAT, 10);
 	// u8 reg_val = mcp251x_read_reg(mcp2515_dev, CANSTAT);
