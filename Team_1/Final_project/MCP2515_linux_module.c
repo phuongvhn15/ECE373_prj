192df1bb569d9596be77047265dc5055fa6ff54e
thanhhuy1509 <nguyenhuy0444@gmail.com>
2022-01-11 22:30:35 +0700

    Nice

--- Team_1/Final_project/MCP2515_linux_module.c
+++ Team_1/Final_project/MCP2515_linux_module.c
@@ -268,15 +268,15 @@ static int __init ModuleInit(void) {
 	// printk("can_dlc: %x, can_id: %x, can_data: %02x %02x %02x %02x ", can_frame_rx.can_dlc, can_frame_rx.can_id, can_frame_rx.can_data[0],can_frame_rx.can_data[1],can_frame_rx.can_data[2],can_frame_rx.can_data[3]);
 
 	can_frame_tx.can_id = 0x58;
-	can_frame_tx.can_dlc = 4;
-	can_frame_tx.can_data[0] = 0x03;
-	can_frame_tx.can_data[1] = 0x22;
-	can_frame_tx.can_data[2] = 0xf1;
-	can_frame_tx.can_data[3] = 0x80;
-	can_frame_tx.can_data[4] = 0;
-	can_frame_tx.can_data[5] = 0;
-	can_frame_tx.can_data[6] = 0;
-	can_frame_tx.can_data[7] = 0;
+	can_frame_tx.can_dlc = 8;
+	can_frame_tx.can_data[0] = 0x02;
+	can_frame_tx.can_data[1] = 0x10;
+	can_frame_tx.can_data[2] = 0x01;
+	can_frame_tx.can_data[3] = 0x00;
+	can_frame_tx.can_data[4] = 0x00;
+	can_frame_tx.can_data[5] = 0x00;
+	can_frame_tx.can_data[6] = 0x00;
+	can_frame_tx.can_data[7] = 0x00;
 	printk("Sending CAN message ");
 	sendMessage(mcp2515_dev_spi, &can_frame_tx);
 
